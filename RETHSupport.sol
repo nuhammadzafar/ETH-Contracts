896c1df0464e2b4cf1c8074f4b3ad79ba8349f7a
MALINGA <104059677+malinga-DIG@users.noreply.github.com>
2022-09-15 15:53:41 +0530

    Update RETHSupport.sol

--- RETHSupport.sol
+++ RETHSupport.sol
@@ -3,7 +3,7 @@
 
 // OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
 
 /**
  * @dev Provides information about the current execution context, including the
@@ -381,14 +381,61 @@ library ECDSA {
         return keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
     }
 }
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        _nonReentrantBefore();
+        _;
+        _nonReentrantAfter();
+    }
+
+    function _nonReentrantBefore() private {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+    }
+
+    function _nonReentrantAfter() private {
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
 
 interface RealmsOfEthernity {
     function Swapin(bytes32 txhash, address account, uint256 amount) external returns (bool) ;
   
 }
 
-contract RETHSupportTransaction is Ownable
-{
+contract RETHSupportTransaction is Ownable, ReentrancyGuard {
     using ECDSA for bytes32;
     address private _signerAddress;
     mapping(bytes=>bool) public usedSigns;
@@ -421,7 +468,7 @@ contract RETHSupportTransaction is Ownable
         _;
     }
 
-    function swappIN (uint256 amount , bytes calldata signature, bytes32 txhash) public whenSwapIsOn returns (bool) {
+    function swappIN (uint256 amount , bytes calldata signature, bytes32 txhash) public whenSwapIsOn nonReentrant returns (bool) {
         require(usedSigns[signature]==false, "Duplicate");
         require(_signerAddress == keccak256(
             abi.encodePacked(
